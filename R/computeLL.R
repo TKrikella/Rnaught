14ec98c9bfc49f963e30159dc7db9f554088fb44
hannajankowski <hkj@yorku.ca>
2022-06-09 17:57:31 -0400

    Testing testing 123

---
+++ R/computeLL.R
@@ -0,0 +1,30 @@
+#' WP method background function computeLL
+#'
+#' This is a background/internal function called by \code{WP}.  It computes the log-likelihood.
+#'
+#' @param NT vector of case counts
+#' @param p discretized version of the serial distribution 
+#' @param R0 basic reproductive ratio
+#' @return The function returns the variable \code{LL} which is the log-likelihood at the input variables and parameters.
+
+#' @export
+# 
+computeLL	<-	function(p, NT, R0){
+
+	k		<-	length(p)
+	TT		<-	length(NT)-1
+	mu_t	<-	rep(0, TT)
+	for(i in 1:TT){
+		Nt		<-	NT[i:max(1,i-k+1)]
+#		print(Nt)
+#		print(p[1:min(k,i)])
+		mu_t[i]	<-	sum(p[1:min(k,i)]*Nt)	
+	}
+	mu_t	<-	R0*mu_t
+	LL		<-	-sum(mu_t)+sum(NT[-1]*log(mu_t))
+
+	return(LL)
+	
+}
+
+
---
+++ R/seqB.R
@@ -0,0 +1,129 @@
+#' seqB method
+#'
+#' This function implements a sequential Bayesian estimation method of R0 due to Bettencourt and Riberio (PloS One, 2008).  See details for important implementation notes.
+#'
+#'The method sets a uniform prior distribution on R0 with support (possible values) set from zero to \code{kappa}.  Our approach discretizes the distribution of R0 on a fine grid.   The distribution of R0 is then updated sequentially, with one update for each new case count observation.  The final estimate of R0 is \code{Rhat}, the mean of the (last) posterior distribution.
+#' The prior distribution is the initial belief of the distribution of R0; which in this implementation is the uninformative uniform distribution with values between zero and kappa.  Users can change the value of kappa only (ie. the prior distribution cannot be changed from the uniform).  As more case counts are observed, the influence of the prior distribution should lessen on the final estimate \code{Rhat}.
+#'
+#' This method is based on an approximation of the SIR model, which is most valid at the beginning of an epidemic.  The method assumes that the mean of the serial distribution (sometimes called the serial interval) is known.  The final estimate can be quite sensitive to this value, so sensitivity testing is recommended. Users should be careful about units of time (e.g. are counts observed daily or weekly?) when implementing.  
+#'
+#' Our code has been modified to provide an estimate even if case counts equal to zero are present in some time intervals.  This is done by grouping the counts over such periods of time.  Without grouping, and in the presence of zero counts, no estimate can be provided.
+#'
+#' @param NT Vector of case counts
+#' @param mu Mean of the serial distribution (needs to match case counts in time units; for example, if case counts are weekly and the serial distribution has a mean of seven days, then mu should be set to 1)
+#' @param kappa Largest possible value of uniform prior, defaults to 20.    
+#'
+#' @return secB returns a list containing the following components:  \code{Rhat} is the estimate of R0 (the posterior mean), \code{posterior} is the posterior distribution of R0 from which alternate estimates can be obtained (see examples), \code{group} is an indicator variable (if \code{group=TRUE}, zero values of NT were input and grouping was done to obtain \code{Rhat}), and \code{inputs} is a list of the original input variables \code{NT, gamma, kappa}.  The variable \code{posterior} is returned as a list made up of \code{supp} the support of the distribution and \code{pmf} the probability mass function.
+#'
+#' @examples
+#' 
+#' ## ===================================================== ##
+#' ## Illustrate on weekly data                             ##
+#' ## ===================================================== ##
+#'
+#' NT <- c(1, 4, 10, 5, 3, 4, 19, 3, 3, 14, 4)	
+#' ## obtain Rhat when serial distribution has mean of five days
+#' res1 <- seqB(NT=NT, mu=5/7)	
+#' res1$Rhat
+#' ## obtain Rhat when serial distribution has mean of three days
+#' res2	<- seqB(NT=NT, mu=3/7)	
+#' res2$Rhat
+#'
+#' ## ===================================================== ##
+#' ## Compute posterior mode instead of posterior mean      ##
+#' ## ===================================================== ##
+#'
+#' Rpost <-	res1$posterior
+#' loc <- which(Rpost$pmf==max(Rpost$pmf))
+#' Rpost$supp[loc]		# posterior mode
+#' res1$Rhat		# compare with posterior mean
+#'
+#' par(mfrow=c(2,1), mar=c(2,2,1,1))
+#' plot(Rpost$supp, Rpost$pmf, col="black", type="l", xlab="", ylab="")
+#' abline(h=1/(20/0.01+1), col="red")
+#' abline(v=res1$Rhat, col="blue")
+#' abline(v=Rpost$supp[loc], col="purple")
+#' legend("topright", legend=c("prior", "posterior", "posterior mean (Rhat)", "posterior mode"), col=c("red", "black", "blue", "purple"), lty=1)
+#' plot(Rpost$supp, Rpost$pmf, col="black", type="l", xlim=c(0.5,1.5), xlab="", ylab="")
+#' abline(h=1/(20/0.01+1), col="red")
+#' abline(v=res1$Rhat, col="blue")
+#' abline(v=Rpost$supp[loc], col="purple")
+#' legend("topright", legend=c("prior", "posterior", "posterior mean (Rhat)", "posterior mode"), col=c("red", "black", "blue", "purple"), lty=1)
+#'
+#' ## ========================================================= ##
+#' ## Compute Rhat using only the first five weeks of data      ##
+#' ## ========================================================= ##
+#'
+#' 
+#' res3 <- seqB(NT=NT[1:5], mu=5/7)	# serial distribution has mean of five days
+#' res3$Rhat
+#' @export
+
+seqB		<-	function(NT, mu, kappa=20){
+	
+	gamma	<-	1/mu
+	
+	if(length(NT)<2) {
+		print("Warning: length of NT should be at least two.")
+	}
+	else{
+	if(min(NT)>0){
+		times		<-	1:length(NT)		
+		tau			<-	diff(times)  
+	}
+	
+	group			<-	FALSE
+	
+	if(min(NT)==0){
+		times	<-	which(NT>0)
+		NT		<-	NT[times]
+		tau		<-	diff(times)
+		group	<-	TRUE
+	}
+	
+	R			<-	seq(0, kappa, 0.01)
+	prior0		<-	rep(1,kappa/0.01+1)
+	prior0		<-	prior0/sum(prior0)
+	
+	k			<-	length(NT)-1
+	R0.post		<-	matrix(0, nrow=k, ncol=length(R))
+	
+	prior		<-	prior0
+	posterior	<-	seq(0, length(prior0))
+	
+	for(i in 1:k){
+		
+	mm1			<-	NT[i]			
+	mm2			<-	NT[i+1]		
+	lambda		<-	tau[i]*gamma*(R-1)
+	lambda		<-	log(mm1)+lambda
+	loglik		<-	mm2*lambda-exp(lambda)
+			
+# numerical issues solve???
+
+#	if((mm1==0)*(mm2==0)==0){
+	maxll			<-	max(loglik)
+	const			<-	0
+	if(maxll>700){
+	const			<-	maxll-700		
+		}	
+	loglik			<-	loglik-const
+#	}
+	
+# end numerical issues solve???
+	
+	posterior	<-	exp(loglik)*prior
+	posterior	<-	posterior/sum(posterior)
+
+	
+	prior		<-	posterior
+		
+	}
+	
+	Rhat		<-	sum(R*posterior)
+	
+return(list(Rhat=Rhat, posterior=list(supp=R, pmf=posterior), group=group, inputs=list(NT=NT, mu=mu, kappa=kappa)))
+}	
+}
+
+
