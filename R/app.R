e467a82539557b156b8d348d6cd0947cdcd1fc44
Naeem Model <me@nmode.ca>
2024-05-09 20:34:03 +0000

    Add Shiny app dependencies add specify entry point

--- R/app.R
+++ R/app.R
@@ -4,16 +4,33 @@
 #'
 #' @export
 app <- function() {
+  missing_pkgs <- c()
+  # Check for any missing, required packages.
   if (!requireNamespace("shiny", quietly = TRUE)) {
-    answer <- readline(paste0("The package 'shiny' must be installed to ",
-      "launch the Rnaught web application.\nWould you like to install it? ",
-      "[Y/n] "))
-
-    if (answer == "Y" || answer == "y")
-      install.packages("shiny")
-    else
-      stop("Aborting.", call. = FALSE)
+    missing_pkgs <- c(missing_pkgs, "shiny")
+  }
+  if (!requireNamespace("bslib", quietly = TRUE)) {
+    missing_pkgs <- c(missing_pkgs, "bslib")
+  }
+  if (!requireNamespace("DT", quietly = TRUE)) {
+    missing_pkgs <- c(missing_pkgs, "DT")
   }
 
-  shiny::shinyApp(ui(), server)
+  # If any of the required packages are missing,
+  # prompt the user to install them.
+  if (length(missing_pkgs) > 0) {
+    cat("The following packages must be installed to run the",
+      "Rnaught web application:\n"
+    )
+    writeLines(missing_pkgs)
+    answer <- readline("Begin installation? [Y/n] ")
+
+    if (answer == "Y" || answer == "y") {
+      install.packages(missing_pkgs)
+    } else {
+      stop("Aborting due to missing, required packages.", call. = FALSE)
+    }
+  }
+
+  shiny::runApp(appDir = system.file("app", package = "Rnaught"))
 }
